fdd7068e93d1d636454c80336525d403d592282a
Suganya.Jagadeesan <suganya.jagadeesan@ten10.com>
2018-03-09 10:09:05 +0000

    Added AddStudentService.java

--- src/test/java/net/serenity/bdd/steps/StudentApiSteps.java
+++ src/test/java/net/serenity/bdd/steps/StudentApiSteps.java
@@ -1,60 +1,41 @@
 package net.serenity.bdd.steps;
 
-import io.restassured.specification.RequestSpecification;
-import net.serenitybdd.core.Serenity;
-import net.serenitybdd.rest.SerenityRest;
+import net.serenity.bdd.ui.AddStudentService;
 import net.thucydides.core.annotations.Step;
-import io.restassured.response.Response;
-import io.restassured.RestAssured;
-import org.json.JSONObject;
-import static org.hamcrest.Matchers.*;
 
 public class StudentApiSteps {
-private Response response;
-private RequestSpecification request;
-private Response post;
+
+    private final AddStudentService addStudentService;
+    public StudentApiSteps() {
+        addStudentService = new AddStudentService();
+    }
 
     @Step
     public void getStudentApi() {
-        response = RestAssured.given().header("auth","Sh@r3dSe3cr3t").
-                get("http://localhost:54802/api/student");
-         }
+        addStudentService.getStudent();
+    }
 
     @Step
     public void checkStatusCode() {
-        response.then().assertThat().statusCode(200);
     }
 
     @Step
     public void getStudentDetails() {
-        response.then().log().all();
+        addStudentService.getStatusLog();
     }
 
+    @Step
     public void newStudentDetail() {
-        RestAssured.baseURI ="http://localhost:54802/api/student";
-        request = RestAssured.given().header("auth","Sh@r3dSe3cr3t");
-
-        JSONObject requestParams = new JSONObject();
-
-        requestParams.put("firstName", "Api"); // Cast
-        requestParams.put("lastName", "Test");
-        requestParams.put("year", "2");
-
-        request.header("Content-Type", "application/json");
-
-        // Add the Json to the body of the request
-        request.body(requestParams.toString());
+        addStudentService.newStudent();
     }
 
+    @Step
     public void postNewStudent() {
-        // Post the request and check the response
-        post = request.post("http://localhost:54802/api/student");
-        int statusCode = response.getStatusCode();
-
+        addStudentService.postStudent();
     }
 
+    @Step
     public void checkNewStudent() {
-        post.then().assertThat().body("firstName", containsString ("Api"));
     }
 
 
---
+++ src/test/java/net/serenity/bdd/ui/AddStudentService.java
@@ -0,0 +1,52 @@
+package net.serenity.bdd.ui;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import net.serenitybdd.core.pages.PageObject;
+import org.json.JSONObject;
+
+import static org.hamcrest.Matchers.containsString;
+
+public class AddStudentService {
+
+    private Response response;
+    private RequestSpecification request;
+    private Response post;
+
+    public void getStudent() {
+        response = RestAssured.given().header("auth", "Sh@r3dSe3cr3t").
+                get("http://localhost:54802/api/student");
+    }
+
+    public void getStatusLog() {
+        response.then().assertThat().statusCode(200);
+        response.then().log().all();
+    }
+
+
+    public void newStudent() {
+        RestAssured.baseURI = "http://localhost:54802/api/student";
+        request = RestAssured.given().header("auth", "Sh@r3dSe3cr3t");
+
+        JSONObject requestParams = new JSONObject();
+
+        requestParams.put("firstName", "Api"); // Cast
+        requestParams.put("lastName", "Test");
+        requestParams.put("year", "2");
+
+        request.header("Content-Type", "application/json");
+
+        // Add the Json to the body of the request
+        request.body(requestParams.toString());
+    }
+
+    public void postStudent() {
+        // Post the request and check the response
+        post = request.post("http://localhost:54802/api/student");
+        int statusCode = response.getStatusCode();
+        post.then().assertThat().body("firstName", containsString ("Api"));
+
+    }
+
+}
